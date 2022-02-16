d8ac431cde49e3465927a13eca64cb93dae37017
Wilian Pereira <wilian_pereira92@outlook.com>
2022-02-16 18:40:38 -0300

    Colocando o erro no next

--- login.js
+++ login.js
@@ -1,10 +1,10 @@
 const randToken = require('rand-token');
 
-module.exports = async (_req, res, _next) => {
+module.exports = async (_req, res, next) => {
     const token = randToken.generate(16);
     try {
         return res.status(200).json({ token });
     } catch (error) {
-        return res.status(error.status).json({ message: error.message });
+        next(error);
     }
 };

e463b12bfaaa0ae6caa1968c76be44bdc8122e09
Wilian Pereira <wilian_pereira92@outlook.com>
2022-02-16 18:42:18 -0300

    Colocando o erro no next

--- middlewares/validateEmail.js
+++ middlewares/validateEmail.js
@@ -15,6 +15,6 @@ module.exports = (req, res, next) => {
         }
         next();
     } catch (error) {
-        return res.status(error.status).json({ message: error.message });
+        next(error);
     }
 };

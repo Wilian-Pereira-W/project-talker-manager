390977f014ae271cb939c8048ac068561f72ccf2
Wilian Pereira <wilian_pereira92@outlook.com>
2022-02-16 18:47:06 -0300

    Adicionando as funções  no index  do middlewares

--- middlewares/index.js
+++ middlewares/index.js
@@ -3,5 +3,17 @@ const validatePassword = require('./validatePassword');
 const validateToken = require('./validateToken');
 const validateName = require('./validateName');
 const validateAge = require('./validateAge');
+const validateData = require('./validateData');
+const validateRate = require('./validateRate');
+const validateTalk = require('./validateTalk');
 
-module.exports = { validateEmail, validatePassword, validateToken, validateName, validateAge };
+module.exports = { 
+    validateEmail, 
+    validatePassword, 
+    validateToken, 
+    validateName, 
+    validateAge, 
+    validateData,
+    validateRate,
+    validateTalk,
+};

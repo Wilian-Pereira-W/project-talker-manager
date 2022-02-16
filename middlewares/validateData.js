ba9439cf9688e2419811b774b97b8a9ea282729b
Wilian Pereira <wilian_pereira92@outlook.com>
2022-02-16 18:42:09 -0300

    Colocando o erro no next

--- middlewares/validateData.js
+++ middlewares/validateData.js
@@ -4,14 +4,18 @@ module.exports = (req, res, next) => {
         const { watchedAt } = talk;
         const dataFormat = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/i;
         const isValid = dataFormat.test(watchedAt);
+        if (!watchedAt) {
+            return res.status(400).json({
+                message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
+              }); 
+        }
         if (!isValid) {
             return res.status(400).json({
                 message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"',
               }); 
-        }
-        next();
+        } next();
     } catch (error) {
-        return res.status(error.status).json({ message: error.message });
+        next(error);
     }
 };
 

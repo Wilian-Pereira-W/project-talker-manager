bda77dd3354c45e1849c0e1eddc1b2eb9b5e514f
Wilian Pereira <wilian_pereira92@outlook.com>
2022-02-16 18:43:00 -0300

    Colocando o erro no next

--- middlewares/validatePassword.js
+++ middlewares/validatePassword.js
@@ -13,6 +13,6 @@ module.exports = (req, res, next) => {
         }
         next();
     } catch (error) {
-        return res.status(error.status).json({ message: error.message });
+        next(error);
     }
 };

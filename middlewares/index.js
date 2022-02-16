175511c22936da7695ecbcd4670fdeeb365064db
Wilian Pereira <wilian_pereira92@outlook.com>
2022-02-16 15:02:18 -0300

    adicionado as validações no index middlewares

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,4 +1,7 @@
 const validateEmail = require('./validateEmail');
 const validatePassword = require('./validatePassword');
+const validateToken = require('./validateToken');
+const validateName = require('./validateName');
+const validateAge = require('./validateAge');
 
-module.exports = { validateEmail, validatePassword };
+module.exports = { validateEmail, validatePassword, validateToken, validateName, validateAge };
